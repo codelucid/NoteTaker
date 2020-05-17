e11e6a4882ad477d5a1270673e104cbbd2e8ba37
Coop <61427314+codelucid@users.noreply.github.com>
2020-05-17 13:22:19 -0400

    created server.js with index and notes html displaying

---
+++ public/server.js
@@ -0,0 +1,84 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express Application
+const app = express();
+const PORT = 3000;
+
+
+// Set up Express application to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Data
+// ================================================
+let notes = [
+    {
+    title: "title of note one",
+    text: "notes",
+    id: ""
+    },
+    {
+    title: "title of note two",
+    text: "notes",
+    id: ""
+    }
+];
+
+// Routes
+// ===============================================
+app.get("/", function(request, response) {
+    // response.send("Welcome to Note Taker!");
+    response.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.get("/notes", function(request, response) {
+    response.sendFile(path.join(__dirname, "./notes.html"));
+});
+
+// Displays all notes
+app.get("/api/notes", function(request, response) {
+    return response.json(notes);
+});
+
+// Displays a single note, or shows "No notes found"
+app.get("/api/notes/:notes", function(request, response) {
+    // Grabs the selected parameter
+    let chosen = request.params.notes;
+
+    console.log(chosen);
+    
+    // Filter to show only the selected note
+    for (let i = 0; i < notes.length; i++) {
+        if (chosen === notes[i].id) {
+            return response.json(notes[i]);
+        }
+    }
+
+    // Otherwise, displays "No notes found"
+    return response.json(false);
+});
+
+
+// Create new Notes - takes in JSON input
+app.post("/api/notes", function(request, response) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    let newNotes = request.body;
+
+    // Use a RegEx Pattern to remove spaces from newNotes
+    newNotes.id = newNotes.title.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newNotes);
+
+    notes.push(newNotes);
+
+    response.json(newNotes);
+});
+
+// Listener
+// =======================================================
+app.listen(PORT, function() {
+    console.log("Application listening on PORT " + PORT);
+});
